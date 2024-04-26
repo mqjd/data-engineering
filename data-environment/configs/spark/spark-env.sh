088dd031794e9423adc7a3f4e15e65b5d049a44e
mqjd <1378415278@qq.com>
2024-04-26 09:17:04 +0800

    shell add +x

--- data-environment/configs/spark/spark-env.sh
+++ data-environment/configs/spark/spark-env.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# shellcheck disable=SC2155
 export SPARK_DIST_CLASSPATH=$(hadoop --config "$HADOOP_CONF_DIR" classpath)
 export SPARK_MASTER_HOST=hd1
 export SPARK_MASTER_WEBUI_PORT=8001
