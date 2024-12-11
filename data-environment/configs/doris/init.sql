7baa653409738685b7e1b5cb2e8306cf06f96e26
mqjd <1378415278@qq.com>
2024-12-11 22:14:45 +0800

    update doris config

--- data-environment/configs/doris/init.sql
+++ data-environment/configs/doris/init.sql
@@ -2,3 +2,7 @@ SET PASSWORD = PASSWORD('123456');
 alter system add BACKEND "hd1:9050";
 alter system add BACKEND "hd2:9050";
 alter system add BACKEND "hd3:9050";
+
+CREATE SCHEMA de;
+CREATE USER 'de' IDENTIFIED BY '123456';
+GRANT ADMIN_PRIV ON *.*.* TO 'de';
